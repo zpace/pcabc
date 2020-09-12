6ba144a5fe56376c24ff3807f67ac365a0895975
Zachary Pace <zpace21@gmail.com>
2020-09-12 16:00:58 -0500

    remove py35 b/c no fastcore, add explicit dask[dataframe] dep

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'numpy', 'scipy', 'matplotlib', 'numba', 'dask', 
+requirements = ['Click>=7.0', 'numpy', 'scipy', 'matplotlib', 'numba', 'dask[dataframe]', 
                 'astropy', 'pandas', 'fastcore']
 
 setup_requirements = ['pytest-runner', ]
