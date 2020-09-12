131e0098ad1a912d5a36d7d6f7a07bbb025f736f
Zachary Pace <zpace21@gmail.com>
2020-09-12 16:08:14 -0500

    will eliminating module-level imports fix travis? we'll see...

--- pcabc/pcabc.py
+++ pcabc/pcabc.py
@@ -5,7 +5,7 @@ from .conf import *
 from pcabc import utils as ut
 
 from pcabc import csp
-from pcabc import rectify
-from pcabc import cov
-from pcabc import spec2pc
-from pcabc import parest
+#from pcabc import rectify
+#from pcabc import cov
+#from pcabc import spec2pc
+#from pcabc import parest
